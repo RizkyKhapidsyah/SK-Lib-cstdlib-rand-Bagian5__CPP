4d7ef308d6232a6d9915aec16ffc0b54feaff816
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-04-28 11:05:58 +0700

    Initial commit

--- SK-Lib-cstdlib-rand-Bagian5__CPP/Source.cpp
+++ SK-Lib-cstdlib-rand-Bagian5__CPP/Source.cpp
@@ -15,6 +15,7 @@ int main() {
     // Program ini akan membuat urutan yang berbeda dari
     // angka acak pada setiap program yang dijalankan
     // Gunakan waktu saat ini sebagai seed untuk generator acak
+
     srand(time(0));
 
     for (int i = 0; i < 4; i++) {
